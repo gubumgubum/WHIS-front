ce0420e5d7305ecfd3ad48676aea953b3d1dcaa8
jyuuuuu0 <s25023@gsm.hs.kr>
2025-11-30 01:09:40 +0900

    feat: 회원가입 1단계 ui 및 폼 구조 구현

--- src/pages/signup/step1.jsx
+++ src/pages/signup/step1.jsx
@@ -6,22 +6,33 @@ import { Link } from 'react-router-dom';
 
 function Step1Page() {
   return (
-    <div>
-      <div>
-        <img src={logo} alt="Logo" className="w-[126px] h-[86px]" />
-        <p>1단계 : 계정 정보 입력</p>
-        <form>
-          <TextInput placeholder="이메일" />
-          <div>
-            <PasswordInput placeholder="비밀번호" />
-            <p>영문과 숫자를 포함한 8자 이상</p>
-            <PasswordInput placeholder="비밀번호 확인" />
-          </div>
-          <Button type="submit">다음</Button>
-        </form>
-        <p>
-          이미 계정이 있다면,<Link to="/signup-step2">로그인</Link>
-        </p>
+    <div className="flex justify-center items-center h-screen">
+      <div className="w-[480px] h-[466px] bg-[#FFF7F0] flex flex-col items-center justify-center rounded-[2rem] shadow-box gap-[34px]">
+        <div>
+          <img src={logo} alt="Logo" className="w-[126px] h-[86px]" />
+          <p className="font-light text-xs">1단계 : 계정 정보 입력</p>
+        </div>
+        <div className="flex flex-col items-center gap-[15px]">
+          <form className="flex flex-col items-center gap-[15px]">
+            <TextInput placeholder="이메일" />
+            <div className="flex flex-col gap-[30px]">
+              <div>
+                <PasswordInput placeholder="비밀번호" />
+                <p className="font-light text-xs text-[#818181] mt-[5px] mb-[10px]">
+                  영문과 숫자를 포함한 8자 이상
+                </p>
+                <PasswordInput placeholder="비밀번호 확인" />
+              </div>
+              <Button type="submit">다음</Button>
+            </div>
+          </form>
+          <p className="inline-flex text-xs text-[#818181] gap-1">
+            이미 계정이 있다면,
+            <Link to="/signin" className="text-[#AC7F5E]">
+              로그인
+            </Link>
+          </p>
+        </div>
       </div>
     </div>
   );
