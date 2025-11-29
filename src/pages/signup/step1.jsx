82f3f4c6f1a63ae1161b66f36ea23b1a15a9134f
jyuuuuu0 <s25023@gsm.hs.kr>
2025-11-29 20:47:18 +0900

    feat: TextInput 추가

--- src/pages/signup/step1.jsx
+++ src/pages/signup/step1.jsx
@@ -1,4 +1,5 @@
 import logo from '../../assets/img/logo.png';
+import TextInput from '../../components/signin/textInput';
 
 function Step1Page() {
   return (
@@ -6,6 +7,9 @@ function Step1Page() {
       <div>
         <img src={logo} alt="Logo" className="w-[126px] h-[86px]" />
         <p>1단계 : 계정 정보 입력</p>
+        <form>
+          <TextInput placeholder="이메일" />
+        </form>
       </div>
     </div>
   );
