8f12807514b757ec5c4aa30c2232fbf1d8fc9dc5
Beom09 <padok430@gmail.com>
2025-11-29 17:53:01 +0900

    Revert "feat: 로그인 페이지 퍼블리싱"

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-import { Routes, Route } from 'react-router-dom';
-import SigninPage from './pages/signin';
+import './App.css';
 
 function App() {
   return (
-    <Routes>
-      <Route path="/signin" element={<SigninPage />} />
-    </Routes>
+    <div>
+
+    </div>
   );
 }
 
