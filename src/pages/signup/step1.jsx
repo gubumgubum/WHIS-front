2f07c3a24eb74b91a60585e3aee6d585d2e5b340
jyuuuuu0 <s25023@gsm.hs.kr>
2025-11-29 20:41:20 +0900

    feat: whis 로고 추가

---
+++ src/pages/signup/step1.jsx
@@ -0,0 +1,13 @@
+import logo from '../../assets/img/logo.png';
+
+function Step1Page() {
+  return (
+    <div>
+      <div>
+        <img src={logo} alt="Logo" className="w-[126px] h-[86px]" />
+      </div>
+    </div>
+  );
+}
+
+export default Step1Page;
