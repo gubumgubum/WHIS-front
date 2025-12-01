f7f1a8f43e10630dc348e94e26f930139d6b26fa
jyuuuuu0 <s25023@gsm.hs.kr>
2025-12-01 20:23:21 +0900

    feat: 회원가입 2번째 단계 페이지 라우트 설정

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import { Routes, Route } from 'react-router-dom';
 import SigninPage from './pages/signin';
 import Step1Page from './pages/signup/step1';
+import Step2Page from './pages/signup/step2';
 
 function App() {
   return (
     <Routes>
       <Route path="/signin" element={<SigninPage />} />
       <Route path="/signup-step1" element={<Step1Page />} />
+      <Route path="/signup-step2" element={<Step2Page />} />
     </Routes>
   );
 }
