35a31ca842cdbaef004f27d7b89819f4a967b0c2
jyuuuuu0 <s25023@gsm.hs.kr>
2025-12-01 20:34:42 +0900

    feat: 파일 업로드 input 추가

--- src/pages/signup/step2.jsx
+++ src/pages/signup/step2.jsx
@@ -1,7 +1,18 @@
+import { useState } from 'react';
 import logo from '../../assets/img/logo.png';
+import Upload from '../../assets/icon/upload';
 import defaultProfile from '../../assets/img/defaultImage.png';
 
 function Step2Page() {
+  const [preview, setPreview] = useState(null);
+
+  const handleImageUpload = (e) => {
+    const file = e.target.files[0];
+    if (!file) return;
+    const imageUrl = URL.createObjectURL(file);
+    setPreview(imageUrl);
+  };
+
   return (
     <div>
       <div>
@@ -13,11 +24,27 @@ function Step2Page() {
           <div>
             <div className="w-[80px] h-[80px] rounded-full overflow-hidden border border-black">
               <img
-                src={defaultProfile}
+                src={preview || defaultProfile}
                 alt="profile"
                 className="w-full h-full object-cover"
               />
             </div>
+
+            <label
+              htmlFor="profileUpload"
+              className="cursor-pointer text-[#AC7F5E] flex items-center gap-1 text-xs"
+            >
+              <Upload />
+              프로필 사진 업로드
+            </label>
+
+            <input
+              type="file"
+              id="profileUpload"
+              accept="image/*"
+              className="hidden"
+              onChange={handleImageUpload}
+            />
           </div>
         </form>
       </div>
