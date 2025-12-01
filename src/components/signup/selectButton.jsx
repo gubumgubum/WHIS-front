e5a4bfb64ff222f7031ade8dd16a45077e8efdc9
jyuuuuu0 <s25023@gsm.hs.kr>
2025-12-01 21:08:25 +0900

    feat: 상태 관리 기능 추가

--- src/components/signup/selectButton.jsx
+++ src/components/signup/selectButton.jsx
@@ -1,8 +1,9 @@
-export default function SelectButton({ className, text }) {
+export default function SelectButton({ className, text, isSelected, onClick }) {
   return (
     <button
       type="button"
-      className={`border border-[#818181] font-light text-lg text-[#818181] rounded-xl focus:bg-[#AC7F5E] focus:text-white focus:font-semibold ${className}`}
+      onClick={onClick}
+      className={`border border-[#818181] rounded-xl ${isSelected ? 'bg-[#AC7F5E] text-white font-semibold' : 'text-[818181] text-lg font-light'} ${className}`}
     >
       {text}
     </button>
