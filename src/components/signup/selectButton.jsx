38817aaed6555c0ec9e1182c878e673676893b60
jyuuuuu0 <s25023@gsm.hs.kr>
2025-12-01 23:06:20 +0900

    feat: 선택 버튼 흰색 배경 추가

--- src/components/signup/selectButton.jsx
+++ src/components/signup/selectButton.jsx
@@ -3,7 +3,7 @@ export default function SelectButton({ className, text, isSelected, onClick }) {
     <button
       type="button"
       onClick={onClick}
-      className={`border border-[#818181] rounded-xl ${isSelected ? 'bg-[#AC7F5E] text-white font-semibold' : 'text-[818181] text-lg font-light'} ${className}`}
+      className={`bg-white border border-[#818181] rounded-xl ${isSelected ? 'bg-[#AC7F5E] text-white font-semibold' : 'text-[818181] text-lg font-light'} ${className}`}
     >
       {text}
     </button>
