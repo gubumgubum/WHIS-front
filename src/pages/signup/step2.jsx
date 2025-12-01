d93c4787f819ad9a8f3597e698b4567b76e923a1
jyuuuuu0 <s25023@gsm.hs.kr>
2025-12-01 20:24:29 +0900

    feat: 로고 추가

---
+++ src/pages/signup/step2.jsx
@@ -0,0 +1,15 @@
+import logo from '../../assets/img/logo.png';
+
+function Step2Page() {
+  return (
+    <div>
+      <div>
+        <div>
+          <img src={logo} alt="Logo" className="w-[126px] h-[86px]" />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Step2Page;
