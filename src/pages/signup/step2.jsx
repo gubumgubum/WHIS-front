7380eee928784ec75a7eec3ee50a87f6b89b006d
jyuuuuu0 <s25023@gsm.hs.kr>
2025-12-01 20:28:00 +0900

    feat: 프로필 이미지 추가

--- src/pages/signup/step2.jsx
+++ src/pages/signup/step2.jsx
@@ -1,4 +1,5 @@
 import logo from '../../assets/img/logo.png';
+import defaultProfile from '../../assets/img/defaultImage.png';
 
 function Step2Page() {
   return (
@@ -8,6 +9,17 @@ function Step2Page() {
           <img src={logo} alt="Logo" className="w-[126px] h-[86px]" />
           <p className="font-light text-xs">2단계 : 프로필 정보 입력</p>
         </div>
+        <form>
+          <div>
+            <div className="w-[80px] h-[80px] rounded-full overflow-hidden border border-black">
+              <img
+                src={defaultProfile}
+                alt="profile"
+                className="w-full h-full object-cover"
+              />
+            </div>
+          </div>
+        </form>
       </div>
     </div>
   );
