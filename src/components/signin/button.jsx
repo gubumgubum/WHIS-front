03d750d9deb0fa415310336ed063ec2068e0bf08
jyuuuuu0 <s25023@gsm.hs.kr>
2025-12-15 10:12:18 +0900

    fix: 버튼 고정 배경색 수정

--- src/components/signin/button.jsx
+++ src/components/signin/button.jsx
@@ -10,7 +10,7 @@ export default function Button({
       type={type}
       onClick={onClick}
       disabled={disabled}
-      className={`w-[25.5rem] h-[3.125rem] bg-[#BA9A81] text-white font-semibold rounded-[1.25rem] ${className}`}
+      className={`w-[25.5rem] h-[3.125rem] text-white font-semibold rounded-[1.25rem] ${className}`}
     >
       {children}
     </button>
