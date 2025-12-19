3b18e5fc94e51901735201e5c650a372731ec39f
jyuuuuu0 <s25023@gsm.hs.kr>
2025-12-19 14:35:26 +0900

    feat: 글 목록

---
+++ src/components/signup/postbox.jsx
@@ -0,0 +1,53 @@
+import Bookmark from '../../assets/icon/bookmark';
+import Comment from '../../assets/icon/comment';
+import Heart from '../../assets/icon/heart';
+import { postlist } from '../../mock/post';
+
+export default function PostBox() {
+  return (
+    <div>
+      {postlist.map((post) => (
+        <div
+          key={post.id}
+          className="flex flex-col w-[544px] h-[210px] bg-white px-[46px] py-10  rounded-[32px] shadow-[6px_6px_15px_1px_rgba(0,0,0,0.15)] gap-[20px]"
+        >
+          <p className="font-pretendard font-medium text-xl text-[#1E0D00]">
+            {post.title}
+          </p>
+          <p className="font-pretendard text-sm text-[#818181] w-[423px] h-[51px]">
+            {post.contents}
+          </p>
+          <div className="flex flex-row justify-between items-center">
+            <div className="flex flex-row gap-1">
+              <p className="font-pretendard text-xs text-[#818181]">
+                {post.user}
+              </p>
+              <p className="text-xs text-[#818181]">-</p>
+              <p className="font-pretendard text-xs text-[#818181]">
+                {post.time}
+              </p>
+              <p className="bg-[#DDA67E] rounded-lg text-white font-pretendard border border-[#818181] text-[10px] w-[32px] h-[15px] text-center">
+                {post.type}
+              </p>
+            </div>
+            <div className="flex flex-row items-center gap-3">
+              <div className="flex items-center gap-1">
+                <Heart />
+                <p className="font-pretendard text-xs text-[#1E0D00]">
+                  {post.like}
+                </p>
+              </div>
+              <div className="flex items-center gap-1">
+                <Comment />
+                <p className="font-pretendard text-xs text-[#1E0D00]">
+                  {post.comment}
+                </p>
+              </div>
+              <Bookmark />
+            </div>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+}
