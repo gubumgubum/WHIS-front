ac537bab8c936f4257eb8fae8ca6c8fc8f92ee62
jyuuuuu0 <s25023@gsm.hs.kr>
2025-12-19 14:12:39 +0900

    feat: 라우트 설정

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Routes, Route } from 'react-router-dom';
 import SigninPage from './pages/signin';
 import Step1Page from './pages/signup/step1';
 import Step2Page from './pages/signup/step2';
+import MainAuthPage from './pages/main-auth';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Route path="/signin" element={<SigninPage />} />
       <Route path="/signup-step1" element={<Step1Page />} />
       <Route path="/signup-step2" element={<Step2Page />} />
+      <Route path="/main-auth" element={<MainAuthPage />} />
     </Routes>
   );
 }
