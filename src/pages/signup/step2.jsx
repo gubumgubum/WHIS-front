f5743c6a4f743cddfa20f773cc565d79617eb775
jyuuuuu0 <s25023@gsm.hs.kr>
2025-12-22 20:16:07 +0900

    feat: api 연동

--- src/pages/signup/step2.jsx
+++ src/pages/signup/step2.jsx
@@ -1,29 +1,66 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import logo from '../../assets/img/logo.png';
 import Upload from '../../assets/icon/upload';
 import defaultProfile from '../../assets/img/defaultImage.png';
 import TextInput from '../../components/signin/textInput';
 import SelectButton from '../../components/signup/selectButton';
 import Button from '../../components/signin/button';
+import { useLocation, useNavigate } from 'react-router-dom';
+import { DEFAULT_PROFILE_URL } from '../../constants/profile';
+import { postSignup } from '../../apis/postSignup';
 
 function Step2Page() {
+  const location = useLocation();
+  const navigate = useNavigate();
+
+  const { email, password } = location.state || {};
+
   const [preview, setPreview] = useState(null);
   const [name, setName] = useState('');
   const [grade, setGrade] = useState('');
 
+  useEffect(() => {
+    if (!email || !password) {
+      navigate('/signup-step1');
+    }
+  }, [email, password, navigate]);
+
   const isFormValid = name.trim() !== '' && grade !== '';
+
+  const gradeMap = {
+    '1학년': '1',
+    '2학년': '2',
+    '3학년': '3',
+  };
 
   const handleImageUpload = (e) => {
     const file = e.target.files[0];
     if (!file) return;
-    const imageUrl = URL.createObjectURL(file);
-    setPreview(imageUrl);
+
+    setPreview(URL.createObjectURL(file));
   };
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
     if (!isFormValid) return;
+
+    const payload = {
+      email,
+      password,
+      name,
+      grade: gradeMap[grade],
+      profileImageUrl: preview || DEFAULT_PROFILE_URL,
+    };
+    try {
+      await postSignup(payload);
+      alert('회원가입이 완료되었습니다.');
+      navigate('/signin');
+    } catch (error) {
+      alert('회원가입에 실패했습니다.');
+      console.error(error);
+    }
   };
+  console.log(process.env.REACT_APP_API_BASE_URL);
 
   return (
     <div className="flex justify-center items-center h-screen">
