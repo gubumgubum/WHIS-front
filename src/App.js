7f021fb611b95b271f70cc4b2e0f1694026b11c2
jyuuuuu0 <s25023@gsm.hs.kr>
2025-12-22 14:50:12 +0900

    feat: toaster 불러오기

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import { Toaster } from 'sonner';
+
 import { Routes, Route } from 'react-router-dom';
 import SigninPage from './pages/signin';
 import Step1Page from './pages/signup/step1';
@@ -5,11 +7,14 @@ import Step2Page from './pages/signup/step2';
 
 function App() {
   return (
-    <Routes>
-      <Route path="/signin" element={<SigninPage />} />
-      <Route path="/signup-step1" element={<Step1Page />} />
-      <Route path="/signup-step2" element={<Step2Page />} />
-    </Routes>
+    <>
+      <Toaster position="top-right" richColors />
+      <Routes>
+        <Route path="/signin" element={<SigninPage />} />
+        <Route path="/signup-step1" element={<Step1Page />} />
+        <Route path="/signup-step2" element={<Step2Page />} />
+      </Routes>
+    </>
   );
 }
 
