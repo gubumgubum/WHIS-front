4450fc98179622e33c110ec67b5e740003f52c44
jyuuuuu0 <s25023@gsm.hs.kr>
2025-12-23 15:03:00 +0900

    feat: 라우트 설정

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Step2Page from './pages/signup/step2';
 import MainUnauthPage from './pages/main-unauth';
 import MainAuthPage from './pages/main-auth';
 import SettingPage from './pages/setting';
+import MyPage from './pages/mypage';
 
 function App() {
   return (
@@ -19,6 +20,7 @@ function App() {
         <Route path="/signup-step2" element={<Step2Page />} />
         <Route path="/setting" element={<SettingPage />} />
         <Route path="/main-auth" element={<MainAuthPage />} />
+        <Route path="/mypage" element={<MyPage />} />
       </Routes>
     </>
   );
