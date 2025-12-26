970b27aaeeacda8328422b02ab59a45033cb76af
Beom09 <padok430@gmail.com>
2025-12-26 18:49:11 +0900

    fix: inquiry 페이지 요소 컴포넌트화

--- src/pages/inquiry/index.jsx
+++ src/pages/inquiry/index.jsx
@@ -1,38 +1,37 @@
 import AuthHeader from '../../components/header/auth/ui';
 import File from '../../assets/icon/file';
+import Flus from '../../assets/icon/flus';
+import TitleInput from './title';
+import ContentTextarea from './content';
 
 function InquiryPage() {
   return (
-    <div className="min-h-screen bg-white">
+    <div className="min-h-screen bg-white flex flex-col">
       <AuthHeader />
 
-      <main className="flex flex-col items-center mt-[120px]">
+      <main className="flex-1 flex flex-col items-center justify-start pt-[120px] pb-[120px]">
         <h1 className="text-xl font-pretendard font-bold mb-8">문의하기</h1>
 
         <section className="w-[550px] h-[545px] bg-[#FFF7F0] rounded-3xl p-6 shadow-lg shadow-black/40">
-          <input
-            type="text"
-            placeholder="제목을 입력하세요"
-            className="placeholder:text-gray-500 w-[500px] h-[50px] px-4 mb-4 border border-gray-400 rounded-xl bg-white text-sm focus:outline-none"
-          />
-
-          <textarea
-            placeholder="내용을 입력하세요"
-            className="placeholder:text-gray-500 w-[500px] h-[300px] p-4 mb-[15px] border border-gray-400 rounded-xl resize-none focus:outline-none"
-          />
+          <TitleInput />
+          <ContentTextarea />
 
           <div className="mb-[15px]">
             <input type="file" id="file-upload" className="hidden" />
 
             <label
               htmlFor="file-upload"
-              className="flex items-center justify-center gap-2
-               w-[130px] h-[50px]
-               border border-gray-400 rounded-xl
-               bg-white text-sm cursor-pointer"
+              className="flex items-center justify-between
+             w-[500px] h-[50px]
+             px-4
+             border border-gray-400 rounded-xl
+             bg-white text-sm cursor-pointer"
             >
-              <File />
-              파일 추가
+              <div className="flex items-center gap-2 text-gray-600">
+                <File />
+                파일 추가
+              </div>
+              <Flus />
             </label>
           </div>
 
