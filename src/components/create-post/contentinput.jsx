16432516af8a5110c42139d893267e4ff3054835
jyuuuuu0 <s25023@gsm.hs.kr>
2025-12-27 03:28:13 +0900

    feat: 내용 입력창 생성

---
+++ src/components/create-post/contentinput.jsx
@@ -0,0 +1,9 @@
+export default function ContentInput() {
+  return (
+    <textarea
+      type="text"
+      placeholder="내용을 입력하세요"
+      className="w-[500px] h-[200px] p-4 border border-[#818181] font-pretendard rounded-xl placeholder:text-[#818181] text-black focus:outline-none"
+    />
+  );
+}
