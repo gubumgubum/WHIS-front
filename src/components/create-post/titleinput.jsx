c8a1123e26bf5292fe7a8fd330a0c31d9b93ae54
jyuuuuu0 <s25023@gsm.hs.kr>
2025-12-27 03:18:30 +0900

    fix: padding 추가

--- src/components/create-post/titleinput.jsx
+++ src/components/create-post/titleinput.jsx
@@ -3,7 +3,7 @@ export default function TitleInput() {
     <input
       type="text"
       placeholder="제목을 입력하세요"
-      className="w-[500px] h-[50px] border border-[#818181] font-pretendard rounded-xl placeholder:text-[#818181] text-black"
+      className="w-[500px] h-[50px] p-4 border border-[#818181] font-pretendard rounded-xl placeholder:text-[#818181] text-black"
     />
   );
 }
