a42aad94d73aefb2effa912c98012711c2a6ba90
jyuuuuu0 <s25023@gsm.hs.kr>
2025-12-26 16:26:42 +0900

    feat: 라우트 설정

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import MainAuthPage from './pages/main-auth';
 import SettingPage from './pages/setting';
 import Alerts from './pages/alerts';
 import MyPage from './pages/mypage';
+import CreatePostPage from './pages/create-post';
 
 function App() {
   return (
@@ -23,6 +24,7 @@ function App() {
         <Route path="/main-auth" element={<MainAuthPage />} />
         <Route path="/alerts" element={<Alerts />} />
         <Route path="/mypage" element={<MyPage />} />
+        <Route path="/create-post" element={<CreatePostPage />} />
       </Routes>
     </>
   );
