bafbceb76951194b90df1d1266a1169ca4a13899
jyuuuuu0 <s25023@gsm.hs.kr>
2025-12-26 22:50:03 +0900

    fix: 게시글 자세히 보기 수정

--- src/pages/view-post/index.jsx
+++ src/pages/view-post/index.jsx
@@ -10,6 +10,22 @@ import ReactionBar from '../../components/view-post/reactionbar';
 import Anoymous from '../../components/view-post/anonymous';
 
 export default function ViewPostPage() {
+  const [isAnonymous, setIsAnonymous] = useState(true);
+  const handleCommentSubmit = () => {
+    if (!commentInput.trim()) return;
+
+    setComments((prev) => [
+      ...prev,
+      {
+        id: Date.now(),
+        content: commentInput,
+        name: isAnonymous ? '익명' : '이하경',
+      },
+    ]);
+
+    setCommentInput('');
+  };
+
   const [comments, setComments] = useState([
     {
       id: 1,
@@ -20,29 +36,13 @@ export default function ViewPostPage() {
 
   const [commentInput, setCommentInput] = useState('');
 
-  const handleCommentSubmit = () => {
-    if (!commentInput.trim()) return;
-
-    setComments((prev) => [
-      ...prev,
-      {
-        id: Date.now(),
-        content: commentInput,
-        name: '익명',
-      },
-    ]);
-
-    setCommentInput('');
-  };
-
   return (
     <div>
       <AuthHeader />
-
-      <div className="flex justify-center pt-[46px] pb-[80px] pl-[385px] pr-[107px]">
-        <div className="flex w-[1200px]">
+      <div className="flex pt-[46px] pb-[34px] pl-[385px] pr-[107px]">
+        <div className="flex w-[1429px]">
           {/* ===== 왼쪽 : 게시글 ===== */}
-          <section className="flex-1 pr-[88px]">
+          <section className="flex flex-col pr-[88px]">
             <div className="flex justify-between items-center">
               <h1 className="text-[36px] font-pretendard font-semibold">
                 전공 선택 어려워요
@@ -73,7 +73,7 @@ export default function ViewPostPage() {
               <ReactionBar initialLike={25} commentCount={comments.length} />
             </div>
 
-            <div className="mt-[40px] leading-7 whitespace-pre-line">
+            <div className="font-pretendard mt-[40px] leading-7 whitespace-pre-line max-w-[500px]">
               전공을 정하는 게 왜 이렇게까지 어려운 일인지 도무지 모르겠어요.
               그냥 하나 고르면 되는 문제인 것 같은데, 막상 진짜로 선택해야
               한다고 생각하면 머릿속이 하얘지고 아무것도 결정할 수 없게 돼요.
@@ -115,8 +115,8 @@ export default function ViewPostPage() {
           <div className="w-px bg-[#E5E5E5]" />
 
           {/* ===== 오른쪽 : 댓글 ===== */}
-          <section className="flex-1 pl-[88px]">
-            <div className="flex flex-col gap-7">
+          <section className="flex-1 pl-[88px] mt-[42px]">
+            <div className="flex flex-col gap-7 h-[717px]">
               {comments.map((comment) => (
                 <CommentItem
                   key={comment.id}
@@ -127,28 +127,33 @@ export default function ViewPostPage() {
             </div>
 
             {/* 댓글 작성 */}
-            <div className="mt-[32px] border-t pt-[28px]">
-              <p className="font-pretendard font-medium mb-[18px]">
+            <div className="fixed bottom-[34px] right-[115px] w-[741px] border-t border-[#818181] bg-white pt-[20px] z-50 ">
+              <p className="font-pretendard font-medium mb-[10px] ml-[62px]">
                 게시글 댓글 작성하기
               </p>
 
               {/* 사용자 정보 */}
-              <div className="flex items-center gap-[13px] mb-[14px]">
+              <div className="flex items-center gap-[13px] mb-[15px] ml-[62px]">
                 <img
                   src={defultimg}
                   alt="프로필"
                   className="w-10 h-10 rounded-full border border-black"
                 />
-                <span className="font-pretendard font-medium">이하경</span>
+                <span className="font-pretendard font-medium">
+                  {isAnonymous ? '익명' : '이하경'}
+                </span>
               </div>
 
               {/* 입력 영역 */}
-              <div className="flex items-center gap-[10px]">
-                <Anoymous isAnonymous={true} />
+              <div className="flex items-center gap-[13px]">
+                <Anoymous
+                  isAnonymous={isAnonymous}
+                  onToggle={() => setIsAnonymous((prev) => !prev)}
+                />
 
                 <input
                   className="flex-1 border-b border-[#818181]
-                 pb-[6px] font-pretendard font-light
+                 pb-[5px] font-pretendard font-light
                  focus:outline-none focus:border-black"
                   placeholder="댓글을 작성하세요..."
                   value={commentInput}
