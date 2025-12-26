36c448a2be54713711ca89c6341146891fe06de1
jyuuuuu0 <s25023@gsm.hs.kr>
2025-12-26 16:53:27 +0900

    fix: svg 경고 수정

--- src/assets/icon/more.jsx
+++ src/assets/icon/more.jsx
@@ -10,8 +10,8 @@ export default function More() {
       <path
         d="M1.5 8.5H1.51V8.51H1.5V8.5ZM1.5 1.5H1.51V1.51H1.5V1.5ZM1.5 15.5H1.51V15.51H1.5V15.5Z"
         stroke="black"
-        stroke-width="3"
-        stroke-linejoin="round"
+        strokeWidth="3"
+        strokeLinejoin="round"
       />
     </svg>
   );
