d90fdef5d1da507b47daec9e73a97286c82139df
Beom09 <padok430@gmail.com>
2025-12-26 21:06:11 +0900

    feat: dtextarea 컴포넌트

---
+++ src/pages/report/Dtextarea.jsx
@@ -0,0 +1,10 @@
+function Dtextarea() {
+  return (
+    <textarea
+      placeholder="내용을 입력하세요"
+      className="font-pretendard placeholder:text-gray-500 w-[500px] h-[300px] p-4 mb-[15px] border border-gray-400 rounded-xl resize-none focus:outline-none"
+    />
+  );
+}
+
+export default Dtextarea;
