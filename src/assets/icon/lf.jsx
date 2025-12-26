109aabc6fa4f9975e5d75cf88b24663f3b4c2ea2
jyuuuuu0 <s25023@gsm.hs.kr>
2025-12-26 16:53:39 +0900

    feat: 줄바꿈 svg 생성

---
+++ src/assets/icon/lf.jsx
@@ -0,0 +1,26 @@
+export default function LF() {
+  return (
+    <svg
+      width="16"
+      height="20"
+      viewBox="0 0 16 20"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M1 1L1 10C1 12.76 3.24 15 6 15H12"
+        stroke="black"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <path
+        d="M11 19L15 15L11 11"
+        stroke="black"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+    </svg>
+  );
+}
