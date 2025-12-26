a8b1e0c65871b1f178f7176adbfa297b98c4542a
jyuuuuu0 <s25023@gsm.hs.kr>
2025-12-27 03:15:46 +0900

    feat: 제목 입력창 컴포넌트화

---
+++ src/components/create-post/titleinput.jsx
@@ -0,0 +1,9 @@
+export default function TitleInput({ title }) {
+  return (
+    <input
+      type="text"
+      placeholder="제목을 입력하세요"
+      className="w-[500px] h-[50px] border border-[#818181] font-pretendard rounded-xl placeholder:text-[#818181] text-black"
+    />
+  );
+}
