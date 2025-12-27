9fde41d52a0bcfc1b09ef88ab360e214f9b89db6
jyuuuuu0 <s25023@gsm.hs.kr>
2025-12-28 02:58:00 +0900

    fix: value, onChange props

--- src/components/create-post/contentinput.jsx
+++ src/components/create-post/contentinput.jsx
@@ -1,9 +1,11 @@
-export default function ContentInput() {
+export default function ContentInput({ value, onChange }) {
   return (
     <textarea
       type="text"
       placeholder="내용을 입력하세요"
       className="w-[500px] h-[200px] p-4 border border-[#818181] font-pretendard rounded-xl placeholder:text-[#818181] text-black focus:outline-none"
+      value={value}
+      onChange={(e) => onChange(e.target.value)}
     />
   );
 }
