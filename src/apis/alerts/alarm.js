2dbab259f380966cb2e812dace7bbe6e90c57d0e
jyuuuuu0 <s25023@gsm.hs.kr>
2025-12-27 15:58:47 +0900

    feat: 알람 전체 조회, 읽음 안읽음 필터링, 읽음 처리 API 생성

---
+++ src/apis/alerts/alarm.js
@@ -0,0 +1,12 @@
+import instance from '../axois'; // 네 axios 인스턴스
+
+export const getAllAlarms = (userId) =>
+  instance.get(`/alarm/all?userId=${userId}`);
+
+export const getFilteredAlarms = (userId, isRead) =>
+  instance.get(`/alarm/filter?userId=${userId}&isRead=${isRead}`);
+
+// 알림 읽음 처리
+export const readAlarm = (alarmId) => {
+  return instance.patch(`/alarm/read/${alarmId}`);
+};
