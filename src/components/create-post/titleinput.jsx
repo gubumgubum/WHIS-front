9544c73210bd604ab81210657f773bc10fec9dae
jyuuuuu0 <s25023@gsm.hs.kr>
2025-12-28 02:58:05 +0900

    fix: value, onChange props

--- src/components/create-post/titleinput.jsx
+++ src/components/create-post/titleinput.jsx
@@ -1,9 +1,11 @@
-export default function TitleInput() {
+export default function TitleInput({ value, onChange }) {
   return (
     <input
       type="text"
       placeholder="제목을 입력하세요"
       className="w-[500px] h-[50px] p-4 border border-[#818181] font-pretendard rounded-xl placeholder:text-[#818181] text-black focus:outline-none"
+      value={value}
+      onChange={(e) => onChange(e.target.value)}
     />
   );
 }
