eed70b04d937f099730b52b53a3d3cf42f99eb66
jyuuuuu0 <s25023@gsm.hs.kr>
2025-12-28 22:48:37 +0900

    feat: api

--- src/apis/alerts/alarm.js
+++ src/apis/alerts/alarm.js
@@ -5,8 +5,9 @@ export const getAllAlarms = (userId) =>
 
 export const getFilteredAlarms = (userId, isRead) =>
   instance.get(`/alarm/filter?userId=${userId}&isRead=${isRead}`);
-
 // 알림 읽음 처리
 export const readAlarm = (alarmId) => {
   return instance.patch(`/alarm/read/${alarmId}`);
+  // 또는
+  // return instance.post(`/alarm/read/${alarmId}`);
 };
