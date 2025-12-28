b58948767e6e432178d996c6cf2fa02875b7381a
jyuuuuu0 <s25023@gsm.hs.kr>
2025-12-28 18:24:56 +0900

    feat: section2,3 불러오기

--- src/pages/about/index.jsx
+++ src/pages/about/index.jsx
@@ -1,9 +1,13 @@
 import Section1 from '../../components/about/section1';
+import Section2 from '../../components/about/section2';
+import Section3 from '../../components/about/section3';
 
 export default function AboutPage() {
   return (
     <div>
       <Section1 />
+      <Section2 />
+      <Section3 />
     </div>
   );
 }
