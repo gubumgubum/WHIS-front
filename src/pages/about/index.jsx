68bfedcb300e71675d64219c6b0c748a15112746
jyuuuuu0 <s25023@gsm.hs.kr>
2025-12-28 22:11:09 +0900

    feawt: 섹션 불러오기

--- src/pages/about/index.jsx
+++ src/pages/about/index.jsx
@@ -1,6 +1,8 @@
 import Section1 from '../../components/about/section1';
 import Section2 from '../../components/about/section2';
 import Section3 from '../../components/about/section3';
+import Section4 from '../../components/about/section4';
+import Section5 from '../../components/about/section5';
 
 export default function AboutPage() {
   return (
@@ -8,6 +10,8 @@ export default function AboutPage() {
       <Section1 />
       <Section2 />
       <Section3 />
+      <Section4 />
+      <Section5 />
     </div>
   );
 }
