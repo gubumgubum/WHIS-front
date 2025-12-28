13c964e35e4e680df29772b6974d1be8b1566566
Beom09 <padok430@gmail.com>
2025-12-28 19:24:38 +0900

    fix: inquiry 페이지 fix

--- src/pages/inquiry/index.jsx
+++ src/pages/inquiry/index.jsx
@@ -1,5 +1,8 @@
 import AuthHeader from '../../components/header/auth/ui';
 import File from '../../assets/icon/file';
+import ContentTextarea from '../../components/inquiry/content';
+import TitleInput from '../../components/inquiry/title';
+import Plus from '../../assets/icon/Plus';
 
 function InquiryPage() {
   return (
@@ -10,29 +13,25 @@ function InquiryPage() {
         <h1 className="text-xl font-pretendard font-bold mb-8">문의하기</h1>
 
         <section className="w-[550px] h-[545px] bg-[#FFF7F0] rounded-3xl p-6 shadow-lg shadow-black/40">
-          <input
-            type="text"
-            placeholder="제목을 입력하세요"
-            className="placeholder:text-gray-500 w-[500px] h-[50px] px-4 mb-4 border border-gray-400 rounded-xl bg-white text-sm focus:outline-none"
-          />
-
-          <textarea
-            placeholder="내용을 입력하세요"
-            className="placeholder:text-gray-500 w-[500px] h-[300px] p-4 mb-[15px] border border-gray-400 rounded-xl resize-none focus:outline-none"
-          />
+          <ContentTextarea />
+          <TitleInput />
 
           <div className="mb-[15px]">
             <input type="file" id="file-upload" className="hidden" />
 
             <label
               htmlFor="file-upload"
-              className="flex items-center justify-center gap-2
-               w-[130px] h-[50px]
-               border border-gray-400 rounded-xl
-               bg-white text-sm cursor-pointer"
+              className="flex items-center justify-between
+                                   w-[500px] h-[50px]
+                                   px-4
+                                   border border-gray-400 rounded-xl
+                                   bg-white text-sm cursor-pointer"
             >
-              <File />
-              파일 추가
+              <div className="flex items-center gap-2 text-gray-600">
+                <File />
+                파일 추가
+              </div>
+              <Plus />
             </label>
           </div>
 
