790afe7b38f6e16ed80cb36250013d9c525afe7a
jyuuuuu0 <s25023@gsm.hs.kr>
2025-12-29 02:18:36 +0900

    feat: auth/me api 연동

--- src/components/mypage/profilecard.jsx
+++ src/components/mypage/profilecard.jsx
@@ -1,11 +1,13 @@
+import { Link } from 'react-router-dom';
+import Setting from '../../assets/icon/setting';
+import { DEFAULT_PROFILE_URL } from '../../constants/profile';
 import { useState } from 'react';
 import Pencil from '../../assets/icon/pencil';
-import defaultProfile from '../../assets/img/defaultImage.png';
-import Setting from '../../assets/icon/setting';
-import { Link } from 'react-router-dom';
 
-export default function ProfileCard() {
-  const [preview, setPreview] = useState(defaultProfile);
+export default function ProfileCard({ me, postCount }) {
+  const [preview, setPreview] = useState(
+    me?.profileImageUrl || DEFAULT_PROFILE_URL,
+  );
 
   const handleImageUpload = (e) => {
     const file = e.target.files[0];
@@ -15,8 +17,11 @@ export default function ProfileCard() {
     setPreview(imageUrl);
   };
 
+  if (!me) return null;
+
   return (
     <div className="flex items-center bg-[#FFF7F0] w-[544px] h-[140px] shadow-[4px_4px_4px_rgba(0,0,0,0.25)] rounded-2xl px-[30px] py-5">
+      {/* 프로필 이미지 */}
       <label
         htmlFor="profileUpload"
         className="relative group cursor-pointer w-[100px] h-[100px]"
@@ -26,15 +31,7 @@ export default function ProfileCard() {
           alt="profile"
           className="w-[100px] h-[100px] rounded-full object-cover border border-black bg-white"
         />
-        <div
-          className="
-          absolute inset-0 rounded-full
-          bg-black/20 w-[100px] h-[100px]
-          flex items-center justify-center
-          opacity-0 group-hover:opacity-100
-          transition-opacity
-        "
-        >
+        <div className="absolute inset-0 rounded-full bg-black/20 flex items-center justify-center opacity-0 group-hover:opacity-100 transition-opacity">
           <Pencil />
         </div>
         <input
@@ -45,17 +42,22 @@ export default function ProfileCard() {
           onChange={handleImageUpload}
         />
       </label>
+
+      {/* 유저 정보 */}
       <div className="flex flex-col gap-1 pl-[18px]">
-        <p className="font-pretendard font-semibold text-2xl">김지유</p>
-        <p className="font-pretendard text-[#818181] text-sm">1학년</p>
+        <p className="font-pretendard font-semibold text-2xl">{me.name}</p>
+        <p className="font-pretendard text-[#818181] text-sm">{me.grade}학년</p>
       </div>
+
+      {/* 게시물 수 */}
       <div className="flex items-center pl-[103px]">
         <div className="flex flex-col gap-1 items-center">
-          <p className="font-pretendard font-semibold text-2xl">10</p>
+          <p className="font-pretendard font-semibold text-2xl">{postCount}</p>
           <p className="font-pretendard font-semibold text-sm text-[#764D2E]">
             게시물
           </p>
         </div>
+
         <Link
           to="/setting"
           className="flex flex-col gap-[6px] items-center pl-[70px]"
